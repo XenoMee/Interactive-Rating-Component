6ecce6effef9338ae4381f7ae8ddeb4b5c5d814e
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-29 20:46:58 +0200

    create rating functionality

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const ratingCardEl = document.getElementById("rating-card");
 const thanksCardEl = document.getElementById("thanks-card");
 const submitRatingButtonEl = document.getElementById("submit-btn");
+const ratingListEl = document.getElementById("rating-list");
+const ratingSelectedEl = document.getElementById("selected-rating");
+const ratingNumbersArray = ratingListEl.children;
+let isSelected = false;
 
 submitRatingButtonEl.addEventListener("click", () => {
-  ratingCardEl.classList.add("hidden");
-  thanksCardEl.classList.remove("hidden");
+  if (isSelected) {
+    ratingCardEl.classList.add("hidden");
+    thanksCardEl.classList.remove("hidden");
+  } else return;
+});
+
+ratingListEl.addEventListener("click", (e) => {
+  const currentRating = e.target.closest("span");
+  if (!currentRating) return;
+  isSelected = true;
+  currentRating.style.backgroundColor = "#FC7614";
+  currentRating.style.color = "#fff";
+  ratingSelectedEl.textContent = `You selected ${currentRating.textContent} out of ${ratingNumbersArray.length}!`;
 });
