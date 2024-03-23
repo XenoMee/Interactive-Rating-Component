3a1c926d813526c1e7f3919dddd26f12e637e8c1
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-23 20:08:46 +0200

    replace aria-selected with data attibute

--- main.js
+++ main.js
@@ -26,13 +26,13 @@ rateAgainBtn.addEventListener('click', displayAppreciationCard);
 
 ratingListEl.addEventListener('click', (e) => {
   const selectedRating = e.target.closest('.rating-item');
-  const selectedBtn = ratingListEl.querySelector('[aria-selected="true"');
+  const selectedBtn = ratingListEl.querySelector('[data-selected="true"');
   if (!selectedRating) return;
 
   if (selectedBtn) {
-    selectedBtn.setAttribute('aria-selected', 'false');
+    selectedBtn.setAttribute('data-selected', 'false');
   }
 
-  selectedRating.setAttribute('aria-selected', 'true');
+  selectedRating.setAttribute('data-selected', 'true');
   ratingSelectedEl.textContent = `You selected ${selectedRating.textContent} out of 5`;
 });
