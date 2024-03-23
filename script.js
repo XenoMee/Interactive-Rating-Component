bfca666fadfb6d560986294be2b40d085c8bf805
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-23 17:04:48 +0200

    refactor click event for the submit button

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const ratingCardEl = document.getElementById("rating-card");
-const thanksCardEl = document.getElementById("thanks-card");
-const submitRatingButtonEl = document.getElementById("submit-btn");
-const ratingListEl = document.getElementById("rating-list");
-const ratingSelectedEl = document.getElementById("selected-rating");
+const ratingCardEl = document.getElementById('rating-card');
+const thanksCardEl = document.getElementById('thanks-card');
+const submitRatingButtonEl = document.getElementById('submit-btn');
+const ratingListEl = document.getElementById('rating-list');
+const ratingSelectedEl = document.getElementById('selected-rating');
 const ratingNumbersArray = ratingListEl.children;
 let isSelected = false;
 
-submitRatingButtonEl.addEventListener("click", () => {
-  if (isSelected) {
-    ratingCardEl.classList.add("hidden");
-    thanksCardEl.classList.remove("hidden");
-    thanksCardEl.classList.add("grid");
-  } else return;
+submitRatingButtonEl.addEventListener('click', (e) => {
+  if (!isSelected) return;
+
+  ratingCardEl.classList.add('hidden');
+  thanksCardEl.classList.remove('hidden');
+  thanksCardEl.classList.add('grid');
 });
 
-ratingListEl.addEventListener("click", (e) => {
-  const selectedRating = e.target.closest("span");
+ratingListEl.addEventListener('click', (e) => {
+  const selectedRating = e.target.closest('span');
   if (!selectedRating) return;
   else {
     isSelected = true;
-    selectedRating.style.color = "#fff";
-    selectedRating.style.backgroundColor = "#FC7614";
+    selectedRating.style.color = '#fff';
+    selectedRating.style.backgroundColor = '#FC7614';
     ratingSelectedEl.textContent = `You selected ${selectedRating.textContent} out of ${ratingNumbersArray.length}`;
   }
 });
